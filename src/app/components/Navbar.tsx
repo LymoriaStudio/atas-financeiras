2a18bdd8c9b147bd765a72de3e645cf90e949d9a
Kelwin Esechiel <kelwin_esechiel@hotmail.com>
2026-06-21 01:50:48 -0300

    fix: btn mobile

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -120,6 +120,7 @@ export function Navbar({ onAdminClick }: NavbarProps) {
           <button
             className="mt-4 w-full flex items-center justify-center gap-2 text-white text-sm px-5 py-2.5 rounded-lg"
             style={{ backgroundColor: "#111827" }}
+               onClick={onAdminClick}
           >
             <LogIn size={15} />
             Entrar no painel
